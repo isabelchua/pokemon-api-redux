b6890e7ca92b9f6e72688e4c31f1e8712e7bbe50
isabelchua <isabelchua86@gmail.com>
2020-11-05 11:27:02 -0800

    connected pokemon page

--- src/containers/Pokemon.js
+++ src/containers/Pokemon.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Pokemon = props => {
-	console.log("props", props);
+	const pokemonName = props.match.params.pokemon;
+	const dispatch = useDispatch();
+	const pokemonState = useSelector(state => state.pokemon);
+
+	console.log("props", pokemonName);
 	return <div>Pokemon</div>;
 };
 
---
+++ src/reducers/PokemonMultipleReducer.js
@@ -0,0 +1,41 @@
+const DefaultState = {
+	loading: false,
+	data: {},
+	errorMsg: ""
+};
+
+// const test = {
+// 	pikachu: {abilities: []},
+// 	balbasaur: {abilities: []}
+// }
+
+const PokemonMultipleReducer = (state = DefaultState, action) => {
+	switch (action.type) {
+		case "POKEMON_MULTIPLE_LOADING":
+			return {
+				...state,
+				loading: true,
+				errorMsg: ""
+			};
+		case "POKEMON_MULTIPLE_FAIL":
+			return {
+				...state,
+				loading: false,
+				errorMsg: "unable to find pokemon"
+			};
+		case "POKEMON_MULTIPLE_SUCCESS":
+			return {
+				...state,
+				loading: false,
+				errorMsg: "",
+				data: {
+					...state.data,
+					[action.pokemonName]: action.payload
+				}
+			};
+		default:
+			return state;
+	}
+};
+
+export default PokemonMultipleReducer;
